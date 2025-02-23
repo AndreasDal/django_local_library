1a49e94a0296ec2012cf8ab01bf3ddaa250dff8d
AndreasDal <guapo_dal@hotmail.com>
2025-02-23 18:55:43 +0100

    Changes made in MDN tutorial Part 5

--- catelog/views.py
+++ catelog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The all() is implied by default
+    num_authors = Author.objects.count()
+
+    # Books containing the word 'the' (case insensitive match)
+    num_books_with_the = Book.objects.filter(title__icontains='the').count()
+
+    # Genres containing the letter 'a' (case insensitive match)
+    num_genres_with_a = Genre.objects.filter(name__icontains='a').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_with_the': num_books_with_the,
+        'num_genres_with_a': num_genres_with_a,
+    }
+
+    # Render the HTML template with the data in the context variable
+    return render(request, 'index.html', context=context)
